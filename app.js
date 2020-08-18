9b3bb6815b6cf8b0250b5962f87094533106d6bb
Kaami Dev <kaami@kaamidev.com>
2020-08-18 00:34:43 -0400

    basic render route

--- app.js
+++ app.js
@@ -10,7 +10,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.post('/render', (req, res) => {
-	res.send('test');
+	if (req.headers.authorization === process.env.AUTH_KEY) {
+		if (req.body.html && req.body.css) {
+		} else {
+			res.status(400).send('Some fields are missing with request.');
+		}
+	} else {
+		res.status(403).send('Auth key missing with request.');
+	}
 });
 
 // listen to port
