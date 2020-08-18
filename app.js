83993d799d80e12189d4d9cd34f6d7cafe1dd2af
Kaami Dev <kaami@kaamidev.com>
2020-08-18 00:52:02 -0400

    got render route working

--- app.js
+++ app.js
@@ -3,30 +3,34 @@ require('dotenv').config();
 const express = require('express');
 const puppeteer = require('puppeteer');
 
-// initialize puppeteer
-const browser = async () => {
-	const browser = await puppeteer.launch();
-	return browser;
-};
-
 // initialize app
 const app = express();
 
 // setup boilerplate
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-app.post('/render', (req, res) => {
-	if (req.headers.authorization === process.env.AUTH_KEY) {
-		if (req.body.html && req.body.css) {
+(async () => {
+	const browser = await puppeteer.launch();
+	app.post('/render', (req, res) => {
+		if (req.headers.authorization === process.env.AUTH_KEY) {
+			if (req.body.html && req.body.css) {
+				(async () => {
+					const page = await browser.newPage();
+					await page.setContent(`<style>${req.body.css}</style><div>${req.body.html}</div>`);
+					const content = await page.$('div');
+					const buffer = await content.screenshot({ type: 'png' });
+					await page.close();
+					res.send('data:image/png;base64,' + buffer.toString('base64'));
+				})();
+			} else {
+				res.status(400).send('Some fields are missing with request.');
+			}
 		} else {
-			res.status(400).send('Some fields are missing with request.');
+			res.status(403).send('Auth key missing with request.');
 		}
-	} else {
-		res.status(403).send('Auth key missing with request.');
-	}
-});
+	});
+})();
 
 // listen to port
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log('Server started and running on port' + PORT));
+app.listen(PORT, () => console.log('Server started and running on port ' + PORT));
